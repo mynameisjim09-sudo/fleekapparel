6f91267d0e244ae463d1e5980783d584aef44fc0
mynameisjim09-sudo <mynameisjim09@gmail.com>
2026-06-10 04:25:55 -0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@tanstack/react-start'
+import { defineConfig } from '@tanstack/react-start/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
