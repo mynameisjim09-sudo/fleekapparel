6a726aa36cd4c8e5f97a7ee3d5c57062ada5b39b
mynameisjim09-sudo <mynameisjim09@gmail.com>
2026-06-10 02:57:54 -0400

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@tanstack/react-start/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  server: {
+    preset: 'vercel',
+  },
   vite: {
     plugins: [
       tsconfigPaths(),
