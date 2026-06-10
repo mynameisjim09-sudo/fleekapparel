941c52a7a17708af26cab8f774c0cc37cbe9700e
mynameisjim09-sudo <mynameisjim09@gmail.com>
2026-06-10 00:22:22 -0400

    Create entry-server.tsx

---
+++ ssr/entry-server.tsx
@@ -0,0 +1,7 @@
+import { createStartHandler, defaultRenderHandler } from '@tanstack/react-start/server'
+import { createRouter } from '../router'
+
+export default createStartHandler({
+  createRouter,
+  render: defaultRenderHandler,
+})
