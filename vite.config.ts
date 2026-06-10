272d0fe6f11c2f38c0d92ed21ba412363f13821e
mynameisjim09-sudo <mynameisjim09@gmail.com>
2026-06-09 23:02:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { tanstackBuildConfig } from "@tanstack/react-start/vite";
-import path from "path";
+import { defineConfig } from 'vite'
+import { tanstackRouterVite } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
-    tanstackBuildConfig(),
+    tanstackRouterVite(),
     react(),
+    tsconfigPaths(),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   }
-});
+})
